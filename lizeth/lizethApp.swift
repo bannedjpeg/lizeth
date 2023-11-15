7e6f0a934b4c9fa4f458b93091eb906ee1fcf928
bannedjpeg <93411980+bannedjpeg@users.noreply.github.com>
2023-11-15 14:40:02 -0800

    Initial Commit

---
+++ lizeth/lizethApp.swift
@@ -0,0 +1,17 @@
+//
+//  lizethApp.swift
+//  lizeth
+//
+//  Created by Jay Duran on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct lizethApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
